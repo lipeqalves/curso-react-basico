c43433ddda8c181f9df5bb136aa9b4011a2b4227
Filipe Alves <lipeqa@gmail.com>
2023-02-23 11:02:26 -0300

    chore:confi-Eslint,prettier, editorconfig,proptyps

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,9 +1,18 @@
-import "./styles.css";
+import P from 'prop-types';
+import './styles.css';
 export const TextInput = ({ searchValue, handleChange }) => {
-  return <input 
-  className="text-input"
-  placeholder="Type your search"
-  type="search" 
-  value={searchValue} 
-  onChange={handleChange} />;
+  return (
+    <input
+      className="text-input"
+      placeholder="Type your search"
+      type="search"
+      value={searchValue}
+      onChange={handleChange}
+    />
+  );
+};
+
+TextInput.propTypes = {
+  searchValue: P.string.isRequired,
+  handleChange: P.func.isRequired,
 };
